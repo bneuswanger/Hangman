acdff4faa5e9f49053fef60b6e1b404569a35b8a
Bryan Neuswanger <bneuswanger@gmail.com>
2022-01-22 14:29:48 -0600

    Successful: Randomly choosing string from the array Looping over the returned string & console.logging the value of each index
    Next up:
    Add creation of letter divs & style them.

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const keys = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '!', '?']
 
 const keyboardDiv = document.getElementById('keyboard');
+const getNewPhraseBtn = document.getElementById('newPhrase');
+
 
 const createKey = () => {
     for (let key of keys) {
@@ -62,18 +64,21 @@ const phrases = [
     },
 ]
 
+getNewPhraseBtn.addEventListener('click', function () {
+    extractLetters(getNewPhrase());
+})
 
-
-
-const pickPhrase = () => {
-    let num = Math.floor(Math.random() * (phrases.length));
-    return num
+const getNewPhrase = () => { //returns string of phrase
+    let num = Math.floor(Math.random() * (phrases.length)); //generates random index position in the array of possible phrases
+    console.log(`index of phrase: ${num}`);
+    console.log(`phrase text: ${phrases[num].text}`)
+    return phrases[num].text 
 }
+// console.log(getNewPhrase()[2])
 
-const getNewPhrase = () => {
-    return phrases[pickPhrase()].text;
-
+function extractLetters(phrase) {
+    for (index of phrase) {
+        console.log(index);
+    }
 }
-
-
 
